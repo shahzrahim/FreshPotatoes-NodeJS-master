2da7a70206cb0b6d51c0d56142b5b59c917ba3d1
Shazi Rahim <shaz.rahim@yahoo.com>
2018-02-09 23:02:47 -0500

    created sequalize database and can make query commands. renders data from films table

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const sqlite = require('sqlite'),
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
+
+//set up database
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: './db/database.db'
+});
+
+
+
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
@@ -26,14 +45,17 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log(req.params.id)
-    console.log('body:', body);
-    res.json(body)
+  // request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
+  //   // console.log('error:', error); // Print the error if one occurred
+  //   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //   console.log(req.params.id)
+  //   console.log('body:', body);
+  //   res.send(body)
+  // })
+  sequelize.query("SELECT * FROM `films`", { type: sequelize.QueryTypes.SELECT})
+  .then(films => {
+    res.send(films)
   })
-
 }
   // res.status(500).send('Not Implemented');
 
