1d4fbbcc2e4f484203367a8821aad9df21baa28f
Shazi Rahim <shaz.rahim@yahoo.com>
2018-02-09 19:38:01 -0500

    grabs value from URL and updates API search w that value

--- index.js
+++ index.js
@@ -12,24 +12,25 @@ Promise.resolve()
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
 // ROUTES
-app.get('/', function(req, res){
-  console.log('this is working')
-  request({
-    method: 'GET',
-    uri: 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=8',
-  }).catch(err, (req, res) => {
+// app.get('/', function(req, res){
+//   console.log('this is working')
+//   request({
+//     method: 'GET',
+//     uri: 'http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=8',
+//   }).catch(err, (req, res) => {
 
-  })
-});
+//   })
+// });
 
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  request('http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=8', function (error, response, body) {
+  request(`http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${req.params.id}`, function (error, response, body) {
     // console.log('error:', error); // Print the error if one occurred
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log(req.params.id)
+    console.log('body:', body);
     res.json(body)
   })
 
